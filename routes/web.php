7bb94c1a35b2e61c437320533ff63cb236b69db9
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2025-03-07 08:40:04 +0700

    Membuat route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,30 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+# Route Prefix
+Route::prefix('category')->group(function () { 
+    Route::get('/food-beverage', function () {
+        return view('food-beverage');
+    });
+    Route::get('/beauty-health', function () {
+        return view('beauty-health');
+    });
+    Route::get('/home-care', function () {
+        return view('home-care');
+    });
+    Route::get('/baby-kid', function () {
+        return view('baby-kid');
+    });
+});
+
+# Route Param
+Route::get('/user/{id}/name/{name}', function ($id, $name) {
+    return view('User dengan nama '.$name.' memiliki id '.$id);
+});
+
+Route::get('/transaction', function () {
+    return view('transaction');
 });
