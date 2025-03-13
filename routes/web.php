2714908e5a63ade3ac854c2650edbb497c1ff7e8
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2025-03-13 10:17:58 +0700

    P3_Praktikum5

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 Route::get('/', [HomeController::class,'index']);
 
@@ -26,3 +27,4 @@ Route::get('/user/{id}/name/{name}',[UserController::class,'index']);
 Route::get('/transaction',[TransactionController::class,'index']);
 
 Route::get('/level',[LevelController::class,'index']);
+Route::get('/kategori',[KategoriController::class,'index']);
