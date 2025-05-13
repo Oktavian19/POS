c57024eeaa33b5ed48e2b4b0af80f3ac49874826
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2025-05-13 15:00:24 +0700

    P8_Tugas 1

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,9 @@ Route::middleware(['auth'])->group(function () { // artinya semua route pada gro
             Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']);
             Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']);
             Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']);
-            Route::delete('/{id}', [LevelController::class, 'destroy']);     
+            Route::delete('/{id}', [LevelController::class, 'destroy']);
+            Route::get('/import', [LevelController::class, 'import']);
+            Route::post('/import_ajax', [LevelController::class, 'import_ajax']); 
         }); 
         
         Route::group(['prefix' => 'user'], function() {     
@@ -56,6 +58,8 @@ Route::middleware(['auth'])->group(function () { // artinya semua route pada gro
             Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); 
             Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); 
             Route::delete('/{id}', [UserController::class, 'destroy']);
+            Route::get('/import', [UserController::class, 'import']);
+            Route::post('/import_ajax', [UserController::class, 'import_ajax']);
         });
     });
 
@@ -98,6 +102,8 @@ Route::middleware(['auth'])->group(function () { // artinya semua route pada gro
             Route::get('/{id}/delete_ajax', [KategoriController::class, 'confirm_ajax']);
             Route::delete('/{id}/delete_ajax', [KategoriController::class, 'delete_ajax']);
             Route::delete('/{id}', [KategoriController::class, 'destroy']);
+            Route::get('/import', [KategoriController::class, 'import']);
+            Route::post('/import_ajax', [KategoriController::class, 'import_ajax']);
         });
         
         Route::group(['prefix' => 'supplier'], function () {
@@ -115,7 +121,9 @@ Route::middleware(['auth'])->group(function () { // artinya semua route pada gro
             Route::put('/{id}/update_ajax', [SupplierController::class, 'update_ajax']);
             Route::get('/{id}/delete_ajax', [SupplierController::class, 'confirm_ajax']);
             Route::delete('/{id}/delete_ajax', [SupplierController::class, 'delete_ajax']);
-            Route::delete('/{id}', [SupplierController::class, 'destroy']);     
+            Route::delete('/{id}', [SupplierController::class, 'destroy']);  
+            Route::get('/import', [SupplierController::class, 'import']);
+            Route::post('/import_ajax', [SupplierController::class, 'import_ajax']);   
         });
     });
 
