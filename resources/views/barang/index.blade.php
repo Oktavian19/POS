f6113f083963d2ec61e3f5c282195426b669d5ad
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2025-05-13 13:47:33 +0700

    P8_Praktikum 3

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -8,6 +8,7 @@
                 <button onclick="modalAction('{{ url('/barang/import') }}')" class="btn btn-info">Import Barang</button> 
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('barang/export_excel') }}"><i class="fa fa-file-excel"></i>Export Barang</a>
                 <button onclick="modalAction('{{ url('barang/create_ajax') }}')" class="btn btn-sm bts-success mt-1">Tambah Ajax</button>
+                <a href="{{ url('/barang/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export Barang</a>
             </div>
         </div>
         <div class="card-body">
