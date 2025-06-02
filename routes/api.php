31b507b0c8d9e0d26309f4620820b3d0c8df82f7
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2025-06-02 20:41:09 +0700

    P10_Praktikum 4

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
